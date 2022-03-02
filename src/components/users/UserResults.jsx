b20c254eb385e62110a1bb30a8050418c6029330
MEGHA2507 <megha.banerjee07@gmail.com>
2022-03-02 17:09:11 +0530

    maintining state of git profile finder using Context in githubContext and accessing using useContext in userResults and Appjs gitcontextProvider

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,33 +1,22 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect } from 'react'
 import Spinner from '../layout/Spinner';
 import UserItem from './UserItem';
+import GitContext from '../../context/githubContext'
 
 function UserResults() {
-
-    const [users, setUsers] = useState([]);
-    const [loading, setLoading] = useState(true);
-
+    const {users, loading, fetchUsers} = useContext(GitContext);
+   
     useEffect(() => {
         fetchUsers()
     }, []);
-    const fetchUsers = async () => {
-        const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-            headers: {
-                Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
-            }
-        })
-
-        const data = await response.json();
-        console.log(data);
-        setUsers(data);
-        setLoading(false);
-    }
+   
     if(!loading){
         return (
             <div className='grid grid-cols-1 gap8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+               
                 {
                     users.map((user) => (
-                        // <h3 key={user.id}>{user.login}</h3>
+                     
                         <UserItem key={user.id} user={user}/>
                     ))
                 }
